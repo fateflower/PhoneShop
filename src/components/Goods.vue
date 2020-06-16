b31ff8bf58db4a85cd9c8eb955b761aa16ccd075
e <zyuanlong@qq.com>
2020-06-16 16:49:11 +0800

    购物车强化班

--- src/components/Goods.vue
+++ src/components/Goods.vue
@@ -1,59 +1,60 @@
 <template>
-	<div class="item">
-		<router-link :to="{
+  <div class="item">
+    <router-link :to="{
 			name:'Detail',
 			params:{
 				id:product._id
 			}
 		}">
-		<img
-		  :src="
-		    product.coverImg
-		      ? product.coverImg
-		      : 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3055768216,2672833891&fm=11&gp=0.jpg'
-		  "
-		  alt
-		/>
-		</router-link>
-		<h5>{{ product.name | spliceStr }}</h5>
-		<p>￥{{ product.price }}</p>
-	</div>
+      <img
+        :src="
+product.coverImg
+? product.coverImg
+: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3055768216,2672833891&fm=11&gp=0.jpg'
+"
+        alt
+      />
+    </router-link>
+    <h5>{{ product.name | spliceStr }}</h5>
+    <p>￥{{ product.price }}</p>
+  </div>
 </template>
 
 <script>
-	export default{
-		props:["product"],
-		filters:{
-			spliceStr(str){
-				return str.length>10?str.substr(0,9)+"...":str
-			}
-		}
-	}
+export default {
+  props: ["product"],
+  filters: {
+    spliceStr(str) {
+      return str.length > 10 ? str.substr(0, 9) + "..." : str;
+    }
+  }
+};
 </script>
 
 <style scoped>
-	.item{
-		width: 40%;
-		padding: 0.5rem;
-		border: solid 0.02rem #ccc;
-		margin-bottom: 0.4rem;
-		display: flex;
-		flex-direction: column;
-	}
-	.item h5{
-		text-align: center;
-		font-size: 1rem;
-	}
-	.item h5,p{
-		padding: 0;
-		margin: 0;
-	}
-	.item img{
-		width: 100%;
-	}
-	.item p{
-		text-align: center;
-		color: red;
-		font-size: 1.2rem;
-	}
+.item {
+  width: 40%;
+  padding: 0.5rem;
+  border: solid 0.02rem #ccc;
+  margin-bottom: 0.4rem;
+  display: flex;
+  flex-direction: column;
+}
+.item h5 {
+  text-align: center;
+  font-size: 1rem;
+}
+.item h5,
+p {
+  padding: 0;
+  margin: 0;
+}
+.item img {
+  width: 100%;
+}
+.item p {
+  text-align: center;
+  color: red;
+  font-size: 1.2rem;
+}
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,62 +1,65 @@
 <template>
-	<div class="nav">	
-		<van-tabbar v-model="active" @change="onChange">
-		  <van-tabbar-item icon="home-o" name="Home">标签1</van-tabbar-item>
-		  <van-tabbar-item icon="fire-o" name="List">标签2</van-tabbar-item>
-		  <van-tabbar-item icon="cart-o" name="Cart">标签3</van-tabbar-item>
-		  <van-tabbar-item icon="user-circle-o" name="User">标签4</van-tabbar-item>
-		</van-tabbar>
-	</div>
+  <div class="nav">
+    <van-tabbar v-model="active" @change="onChange">
+      <van-tabbar-item icon="home-o" name="Home">首页</van-tabbar-item>
+      <van-tabbar-item icon="fire-o" name="List">列表</van-tabbar-item>
+      <van-tabbar-item icon="cart-o" name="Cart">购物车</van-tabbar-item>
+      <van-tabbar-item icon="user-circle-o" name="User">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
-	
-	export default {
-		data() {
-			return {
-				active: "Home"
-			}
-		},
-		methods:{
-			 onChange(index) {
-					this.$router.push({
-						name:this.active
-					}).catch(() => {})
-				},
-		}
-	}
+export default {
+  data() {
+    return {
+      active: "Home"
+    };
+  },
+  methods: {
+    onChange() {
+      this.$router
+        .push({
+          name: this.active
+        })
+        .catch(() => {});
+    }
+  }
+};
 </script>
 
 <style scoped>
-	.nav,.nav ul{
-		width: 100%;
-		height: 46px;
-	}
-	.nav{
-		border-top: solid #999 0.02rem;
-	}
-	.nav ul,.nav li{
-		margin: 0;
-		padding: 0;
-	}
-	.nav ul{
-		display:  flex;
-		align-items: center;
-		justify-content: space-around;
-	}
-	.nav li {
-		list-style: none;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		font-weight: bold;
-		font-size: 0.8rem;
-	}
-	.nav img{
-		width: 24px;
-	}
-	.nav li.active {
-	  color: #d81e06;
-	}
+.nav,
+.nav ul {
+  width: 100%;
+  height: 46px;
+}
+.nav {
+  border-top: solid #999 0.02rem;
+}
+.nav ul,
+.nav li {
+  margin: 0;
+  padding: 0;
+}
+.nav ul {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.nav li {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 0.8rem;
+}
+.nav img {
+  width: 24px;
+}
+.nav li.active {
+  color: #d81e06;
+}
 </style>
