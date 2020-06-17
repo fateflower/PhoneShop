9b426b20adf52913c1eadef6c1a3db27d71224c6
saber <1140325199@qq.com>
2020-06-17 11:41:06 +0800

    one

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
   data() {
     return {
       hideNav: false,
-      activeName:"Home"
+      activeName: "Home"
     };
   },
   components: {
@@ -23,7 +23,7 @@ export default {
       } else {
         this.hideNav = false;
       }
-	  console.log(this.$route)
+      console.log(this.$route);
       this.activeName = this.$route.name;
     }
   }
