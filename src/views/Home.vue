929b86aa27fb0c8fc4d8055692366dde00292973
e <zyuanlong@qq.com>
2020-06-19 10:18:38 +0800

    数据添加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -159,11 +159,43 @@
         </a>
       </div>
     </div>
-	<div class="like">
-		<h1><img src="https://s10.mogucdn.com/mlcdn/c45406/190426_4hlfgkc2ceaea67422ag73077lfce_1611x166.png_1200x9999.v1c7E.81.webp" alt=""></h1>
-	</div>
+    <div class="like">
+      <h1>
+        <img
+          src="https://s10.mogucdn.com/mlcdn/c45406/190426_4hlfgkc2ceaea67422ag73077lfce_1611x166.png_1200x9999.v1c7E.81.webp"
+          alt
+        />
+      </h1>
+    </div>
     <div class="hot">
-      <Goods v-for="item in list" :key="item._id" :product="item"></Goods>
+      <!-- <Goods v-for="item in list" :key="item._id" :product="item"></Goods> -->
+      <ul class="list">
+        <li v-for="item in list" :key="item._id">
+          <dl style="margin-top:10px;float:left">
+            <dt>
+              <a href="www">
+                <img class="tup" :src="item.img" alt />
+              </a>
+            </dt>
+            <div class="xq">
+              <a href="https://www.iliangcang.com/i/goods/?id=280069">
+                <div class="sm">{{item.intro}}</div>
+              </a>
+              <div class="jx">历史价格￥{{item.pre_price}}</div>
+              <div class="price">￥{{item.price}}</div>
+              <button class="car">加入购物车</button>
+            </div>
+          </dl>
+          <!-- <h1 class="title">{{item.title}}</h1>
+          <a href>
+            <img class="imgs" :src="item.img" alt />
+          </a>
+          <p class="intro">{{item.intro}}</p>
+          <p class="hot0">热度:{{item.hot}}</p>
+          <div class="price">￥{{item.price}}</div>
+          <button>购物车</button>-->
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -171,24 +203,35 @@
 <script>
 import Goods from "../components/Goods.vue";
 import { getList } from "../services/products.js";
+import axios from "axios";
 export default {
-  name: "Home",
+  name: "Goods",
   data() {
     return {
       list: []
     };
   },
-  components: {
-    Goods
-  },
-  async created() {
-    const res = await getList(1, 8);
-    this.list = res.data.products;
-  },
+  // components: {
+  //   Goods
+  // },
+  // async created() {
+  //   const res = await getList(1, 8);
+  //   this.list = res.data.products;
+  // },
   methods: {
     closeTop() {
       this.$refs.abc.style.display = "none";
     }
+  },
+  created() {
+    axios.get("http://localhost:3000/goods").then(res => {
+      console.log(res.data);
+      res = res.data;
+      if (res.code === 0) {
+        this.list = res.data;
+        // console.log(res.data);
+      }
+    });
   }
 };
 </script>
@@ -197,11 +240,99 @@ export default {
   margin: 0;
   padding: 0;
 }
+.imgs {
+  width: 48%;
+  height: 100px;
+  text-align: center;
+  margin-left: 45px;
+}
+.list li {
+  width: 50%;
+  float: left;
+}
+.car {
+  line-height: 1.499;
+  position: relative;
+  font-weight: 400;
+  white-space: nowrap;
+  text-align: center;
+  background-image: none;
+  border: 1px solid transparent;
+  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.015);
+  cursor: pointer;
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  touch-action: manipulation;
+  height: 32px;
+  padding: 0 15px;
+  font-size: 10px;
+  border-radius: 4px;
+  color: darkorange;
+  border-color: #d9d9d9;
+  float: right;
+  margin-top: 10px;
+}
+.tup {
+  width: 90%;
+  margin: 0 5%;
+}
+.yb dl {
+  width: 50%;
+  /* padding-bottom: 70px; */
+  /* border: solid 5px rgb(250, 250, 250); */
+}
+.price {
+  padding-left: 10px;
+  margin-top: 0px;
+  float: left;
+  height: 12px;
+  color: #3193f3;
+  /* font-size: 12px; */
+}
+.xq {
+  width: 90%;
+  height: 100px;
+  margin: -4px 5%;
+  box-shadow: 0 0 3px #afaeae;
+  border-radius: 0 0 3px 3px;
+}
+.xq p {
+  width: 90%;
+}
+.sm {
+  font-size: 12px;
+  line-height: 1.5;
+  word-break: break-all;
+  /* height: 58px; */
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  padding-left: 8px;
+  padding-top: 13px;
+  /* font-size: 20px; */
+}
+.jx {
+  padding-left: 15px;
+  margin-top: 7px;
+  font-size: 10px;
+  color: #5b5a5a;
+}
+
+.xz {
+  margin: 10px 8px;
+  height: 25px;
+  width: 25px;
+}
 .ad {
   width: 100%;
 }
 .hot {
-  display: flex;
+  /* display: flex; */
   justify-content: space-around;
   flex-wrap: wrap;
 }
@@ -263,13 +394,14 @@ export default {
   height: 50px;
   margin: 0 auto;
 }
-.like{
-	width: 100%;
-	height: 2.6rem;
+.like {
+  width: 100%;
+  height: 2.6rem;
 }
-.like h1,.like img{
-	width: 100%;
-	height: 100%;
-	display: block;
+.like h1,
+.like img {
+  width: 100%;
+  height: 100%;
+  display: block;
 }
 </style>
